9cb194a69cedeb3a928bb66f69596fedb2ea1859
defkon98 <alex_schmid@hotmail.ch>
2022-01-13 20:43:22 +0100

    slow down planets and fasten them

--- src/Objects/Planet.js
+++ src/Objects/Planet.js
@@ -1,9 +1,10 @@
 class Planet {
     constructor(distanceRadius, canvas) {
       this.sizeRadius = Math.floor(Math.random() * 5) + 10;
-      this.defaultVelocity = ((Math.random() * 20) + 5) / 10000;
+      this.defaultVelocity = ((Math.random() * 15) + 5) / 10000;
       this.currentVelocity = this.defaultVelocity;
       this.interreactionVelocity = 0;
+      this.onHold = false;
       this.winkel = 0;
       this.distanceRadius = distanceRadius;
       this.color = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
@@ -12,11 +13,7 @@ class Planet {
     }
 
     draw(ctx) {
-      // if (this.interreactionVelocity > 0 && this.interreactionVelocity > this.currentVelocity) {
-      //   this.currentVelocity = this.currentVelocity * 1.5;
-      // }else if(this.interreactionVelocity > 0 && this.interreactionVelocity < this.currentVelocity){
-      //   this.currentVelocity = this.currentVelocity / 1.5;
-      // }
+      this.speedUpAndSlowDown(1, 10);
       ctx.beginPath();
       ctx.fillStyle = this.color;
       ctx.arc((this.x - this.distanceRadius) + Math.sin(this.winkel * Math.PI) * this.distanceRadius, this.y + Math.cos(this.winkel * Math.PI) * this.distanceRadius, this.sizeRadius, 0, 2 * Math.PI, false);
@@ -26,6 +23,37 @@ class Planet {
       if(this.winkel > 2){
         this.winkel = 0;
       }
-
     }
+
+    setInterreactionVelocity(speed){
+      //calculate the speed
+      this.interreactionVelocity = speed;
+    }
+
+    speedUpAndSlowDown(actualActionTime, setActionTime){
+      if (actualActionTime >= setActionTime) {
+        if (this.interreactionVelocity > 0) {
+          this.currentVelocity = this.defaultVelocity * this.interreactionVelocity;
+        } else if (this.onHold) {
+          this.currentVelocity = 0;
+        }
+      } else {
+        if (this.interreactionVelocity > 1) {
+          this.currentVelocity = this.currentVelocity * 1.01;
+        }else if(this.interreactionVelocity < this.currentVelocity){
+          this.currentVelocity = this.currentVelocity / 1.01 + this.interreactionVelocity;
+        } else if(this.interreactionVelocity === 0 && this.onHold){
+          this.currentVelocity = this.currentVelocity / 1.01;
+        }
+      }
+    }
+
+    setOnHold(){
+      this.onHold = true;
+    }
+
+    breakOnHold(){
+      this.onHold = false;
+    }
+
 }
