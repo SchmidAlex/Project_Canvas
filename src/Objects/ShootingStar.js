326fce2bd4cd96c60afb01f485b56aa2445a7f61
romanocast <castagnoli@simpit.ch>
2022-01-13 21:03:47 +0100

    Update ShootingStar.js

--- src/Objects/ShootingStar.js
+++ src/Objects/ShootingStar.js
@@ -1,15 +1,17 @@
 class ShootingStar {
-    constructor(size, velocity, color) {
+    constructor(size, velocity, color, canvas) {
       this.size = size;
       this.velocity = velocity;
       this.color = color;
+      this.x = Math.floor(Math.random() * canvas.width);
+      this.y = Math.floor(Math.random() * canvas.width);
       //maybe a direction or something?
     }
 
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(50, 50, this.size, 0, 2 * Math.PI, false);
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
         ctx.fill();
         ctx.closePath();
     }
