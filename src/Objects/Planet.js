5d7eeba4bd9afcf81e554d158f0aecbba6606ee6
defkon98 <alex_schmid@hotmail.ch>
2022-01-13 16:27:09 +0100

    accept changes

--- src/Objects/Planet.js
+++ src/Objects/Planet.js
@@ -1,7 +1,10 @@
 class Planet {
     constructor(distanceRadius) {
       this.sizeRadius = Math.floor(Math.random() * 5) + 10;
-      this.velocity = Math.floor(Math.random() * 100) + 1;
+      this.defaultVelocity = ((Math.random() * 20) + 5) / 10000;
+      this.currentVelocity = this.defaultVelocity;
+      this.interreactionVelocity = 0;
+      this.winkel = 0;
       this.distanceRadius = distanceRadius;
       this.color = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
     }
@@ -12,5 +15,13 @@ class Planet {
       ctx.arc(canvas.width / 2 + this.distanceRadius , canvas.height / 2, this.sizeRadius, 0, 2 * Math.PI, false);
       ctx.fill();
       ctx.closePath();
+      this.winkel += this.currentVelocity;
+      if(this.winkel > 2){
+        this.winkel = 0;
+      }
+    }
+
+    changeVelocity(speed){
+
     }
 }
