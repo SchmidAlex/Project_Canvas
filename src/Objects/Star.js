13882c037ae2e47ee5bc23f81cabdeb02382c256
romanocast <castagnoli@simpit.ch>
2022-01-14 08:27:07 +0100

    made living time of supernova

--- src/Objects/Star.js
+++ src/Objects/Star.js
@@ -1,9 +1,18 @@
 class Star {
+
+    /**
+     * 
+     * @param {Empty array} stars 
+     * @param {Size of the star} starSize 
+     * @param {Canvas object} canvas 
+     */
     constructor(stars, starSize, canvas) {
 
+        // Random x and y position of the star based on the measures of the canvas
         var xPos = Math.floor(Math.random() * (canvas.width - 1)) + 1;
         var yPos = Math.floor(Math.random() * (canvas.height - 1)) + 1;
 
+        // Check if the position isn't already taken by another star
         stars.forEach(star => {
             while (star.x + (starSize / 2) > xPos && star.x - (starSize / 2) < xPos 
                 && star.y + (starSize / 2) > yPos && star.y - (starSize / 2) < yPos) {
@@ -15,15 +24,17 @@ class Star {
             }
         });
 
-        this.xPosition = xPos;
-        this.yPosition = yPos;
+        this.x = xPos;
+        this.y = yPos;
         this.starSize = starSize;
     }
-    //comment
+    //color abweichung TODO
     draw(ctx, starColor) {
         ctx.beginPath();
         ctx.fillStyle = starColor;
-        ctx.arc(this.xPosition, this.yPosition, this.starSize, 0, 2 * Math.PI, false);
+
+        // Draw star
+        ctx.arc(this.x, this.y, this.starSize, 0, 2 * Math.PI, false);
         ctx.fill();
         ctx.closePath();
     }
--- src/Objects/Supernova.js
+++ src/Objects/Supernova.js
@@ -4,20 +4,40 @@ class Supernova {
      * @param {Size of the supernova} size 
      * @param {Color of the supernova} color 
      * @param {Canvas object} canvas 
+     * @param {Durartion of the supernova to appear in space} livingTime
      */
-    constructor(size, color, canvas) {
+    constructor(size, color, canvas, livingTime) {
       this.size = size;
       this.color = color;
 
       // The x Position and y Position of the supernova random calculated based on the canvas dimensions
       this.x = Math.floor(Math.random() * canvas.width);
       this.y = Math.floor(Math.random() * canvas.height);
+
+      // Counter for the living time
+      this.ticker = 0;
+      this.livingTime = livingTime;
     }
 
     draw(ctx) {
       // Set color based on parameter given when initialising supernova class
       ctx.fillStyle = this.color;
       ctx.beginPath();
+
+      // Check if the living time of the supernova is already reached
+      if(this.ticker <= this.livingTime) {
+
+        // Draw supernova
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
+        
+        // Increase position for moving effect when supernova is redrawing itself
+        this.x += 1;
+        this.y += 1;
+
+        // Increase counter
+        this.ticker += 1;
+      }
+
 
       // Draw supernova
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI, false);
