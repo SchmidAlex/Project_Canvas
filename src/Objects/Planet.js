08dd05d9d93b96908543d4704befa3fef22d9914
defkon98 <alex_schmid@hotmail.ch>
2022-01-14 10:57:42 +0100

    get adaptive speeding or slowing

--- src/Objects/Planet.js
+++ src/Objects/Planet.js
@@ -16,14 +16,19 @@ class Planet {
       this.color = "rgb(" + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ", " + Math.floor(Math.random() * 256) + ")";
       this.x = canvas.width / 2 + distanceRadius;
       this.y = canvas.height / 2;
+      this.setActionTime = 5;
+      this.destinationTimeSpeed;
+      this.slowDownOrSpeedUp = false;
     }
 
     /**
      * Draw function for the planet. The function draws the planet on the canvas
      * @param ctx with which we want to draw
      */
-    draw(ctx) {
-      this.speedUpAndSlowDown(1, 1/**+ 10 */);
+    draw(ctx, actualTime) {
+      if (this.slowDownOrSpeedUp) {
+        this.speedUpAndSlowDown(Math.floor(actualTime), this.destinationTimeSpeed);
+      }
       ctx.beginPath();
       ctx.fillStyle = this.color;
       ctx.arc((this.x - this.distanceRadius) + Math.sin(this.winkel * Math.PI) * this.distanceRadius, this.y + Math.cos(this.winkel * Math.PI) * this.distanceRadius, this.sizeRadius, 0, 2 * Math.PI, false);
@@ -41,42 +46,69 @@ class Planet {
      * @param mouseUpPoint X coords, when the user left the left mouse button
      */
     setInterreactionVelocity(mouseDownPoint, mouseUpPoint, mouseTimeDown, mouseTimeUp){
-      console.log(mouseUpPoint / mouseDownPoint / 10);
-      this.interreactionVelocity = mouseUpPoint / mouseDownPoint / 10;
+      // Calculate the holdtime of the mouse in ms
+      let holdTime = mouseTimeUp - mouseTimeDown;
+      // Calculate velocity in px per ms
+      let calculatedVelocity = (mouseUpPoint - mouseDownPoint) / holdTime;
+      this.interreactionVelocity = this.defaultVelocity * calculatedVelocity * 1000;
+      this.destinationTimeSpeed = Math.floor(mouseTimeUp + this.setActionTime * 1000);
+      this.slowDownOrSpeedUp = true;
     }
 
     /**
      * Handels speeding planets up or slowing them down.
      * @param actualActionTime 
-     * @param setActionTime 
+     * @param speedUpOrSlowDownTime 
      */
-    speedUpAndSlowDown(actualActionTime, setActionTime){
-      if (actualActionTime > setActionTime) {
+    speedUpAndSlowDown(actualActionTime, speedUpOrSlowDownTime){
+      if (actualActionTime > speedUpOrSlowDownTime - 10 && actualActionTime < speedUpOrSlowDownTime + 10) { 
+        this.slowDownOrSpeedUp = false;
         if (this.interreactionVelocity > 0) {
-          this.currentVelocity = this.defaultVelocity * this.interreactionVelocity;
-          this.interreactionVelocity = 0;
+          if (this.interreactionVelocity == this.defaultVelocity) {
+            this.currentVelocity = this.defaultVelocity;
+            this.interreactionVelocity = 0;
+          } else {
+            this.currentVelocity = this.defaultVelocity * this.interreactionVelocity;
+            this.interreactionVelocity = this.defaultVelocity;
+            this.slowDownOrSpeedUp = true;
+            this.destinationTimeSpeed = Math.floor(actualActionTime + this.setActionTime * 1000);
+          }
         } else if (this.onHold) {
           this.currentVelocity = 0;
         }
       } else {
-        if (this.interreactionVelocity > 1) {
+        if (this.interreactionVelocity > 1 && this.currentVelocity < this.defaultVelocity * this.interreactionVelocity) {
           this.currentVelocity = this.currentVelocity * 1.01;
-        } else if (this.interreactionVelocity < this.currentVelocity && this.interreactionVelocity != 0) {
-          this.currentVelocity = this.currentVelocity / 1.01 + this.interreactionVelocity;
+        } else if (this.interreactionVelocity < 1 && this.interreactionVelocity != 0 && this.currentVelocity > this.defaultVelocity * this.interreactionVelocity && this.interreactionVelocity != this.defaultVelocity) {
+          this.currentVelocity = this.currentVelocity / 1.01;
         } else if (this.interreactionVelocity === 0 && this.onHold) {
           this.currentVelocity = this.currentVelocity / 1.01;
-        } else if (this.currentVelocity < this.defaultVelocity && this.interreactionVelocity === 0 && !this.onHold) {
-          this.currentVelocity = this.currentVelocity * 1.01;
+        } else if (this.currentVelocity < this.defaultVelocity && this.interreactionVelocity === 0 && !this.onHold) { 
+          if (this.currentVelocity > this.defaultVelocity) {
+            this.currentVelocity = this.defaultVelocity;
+          } else {
+            this.currentVelocity = this.currentVelocity * 1.01;
+          }
+        } else if (this.interreactionVelocity == this.defaultVelocity && this.currentVelocity > this.defaultVelocity) {
+          this.currentVelocity = this.currentVelocity / 1.01;
+        } else if (this.interreactionVelocity == this.defaultVelocity && this.currentVelocity < this.defaultVelocity) {
+          this.currentVelocity = this.currentVelocity * 1.01
+        } else if (this.onHold){
+          this.currentVelocity = this.currentVelocity / 1.01;
         }
       }
     }
 
-    setOnHold(){
+    setOnHold(mouseTimeDown){
       this.onHold = true;
+      this.slowDownOrSpeedUp = true;
+      this.destinationTimeSpeed = Math.floor(mouseTimeDown + this.setActionTime * 1000);
     }
 
-    breakOnHold(){
+    breakOnHold(mouseTimeUp){
       this.onHold = false;
+      this.slowDownOrSpeedUp = true;
+      this.destinationTimeSpeed = Math.floor(mouseTimeUp + this.setActionTime * 1000);
     }
 
 }
